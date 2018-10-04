c5ca07b017de001b23170f44fed539f281d0c6e7
pppppass <lzh2016p@pku.edu.cn>
2018-10-04 09:19:58 +0800

    Update exercises for week 03

--- W03Exercise/Plot.py
+++ W03Exercise/Plot.py
@@ -1,10 +1,11 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[9]:
 
 
 import shelve
+import numpy
 import matplotlib
 matplotlib.use("pgf")
 from matplotlib import pyplot
@@ -18,15 +19,20 @@ with shelve.open("Result") as db:
     rt = db["4result"]
 
 
-# In[17]:
+# In[25]:
 
 
-title = ["<LABEL1~~~>", "<LABEL2~~~>", "LU", "Cholesky", "LDL", "Banded LU"]
+title = ["<LABEL1>", "<LABEL2>", "LU", "Cholesky", "LDL", "Banded LU"]
 pyplot.figure(figsize=(6.0, 4.5))
 for i in range(6):
     pyplot.plot(n, rt[i][0], label=title[i])
+    pyplot.scatter(n, rt[i][0], s=5.0)
+pyplot.plot([numpy.power(10.0, 1.5), 1.0e2], [1.0e1, 1.0e4], linewidth=0.5, color="black", label="Slope $6$")
+pyplot.plot([numpy.power(10.0, 1.5), 1.0e2], [1.0e-3, 1.0e-2], linewidth=0.5, color="black", linestyle="dashed", label="Slope $2$")
 pyplot.legend()
-pyplot.xlabel("$n$")
+pyplot.semilogx()
+pyplot.semilogy()
+pyplot.xlabel("$N$")
 pyplot.ylabel("<LABEL3>")
 pyplot.savefig("Figure1.pgf")
 pyplot.show()
@@ -35,7 +41,7 @@ pyplot.show()
 # In[3]:
 
 
-with open("Table1.tbl", "w") as f:
+with open("Table6.tbl", "w") as f:
     for i in range(len(n)):
         f.write(("{} " + "& {:.3f} " * 6 + "\\\\\n").format(n[i], *(rt[j][0][i] for j in range(6))))
         f.write("\\hline\n")
